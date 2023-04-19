c900fa8ba4750c042220d08b192fca5d89acd804
NJagadeeshReddy <129485318+NJagadeeshReddy@users.noreply.github.com>
2023-04-19 10:43:41 +0530

    calculator basic

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cal from './components/Cal'
+
+const App = () => {
+  return (
+    <div>
+        <h1> <i><u>Simple Calculator</u></i>  </h1>
+      <Cal/>
+    </div>
+  )
+}
+
+export default App
